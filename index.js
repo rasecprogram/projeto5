5e0c2d18bda8cd46bc2c816c7fc30a545b8ac315
rasecprogram <156724062+rasecprogram@users.noreply.github.com>
2024-03-20 22:45:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function realizarOperacoes(num1, num2) {
+    const soma = num1 + num2;
+    const subtracao = num1 - num2;
+    const multiplicacao = num1 * num2;
+    const divisao = num1 / num2;
+
+    console.log(`${num1} + ${num2} = ${soma}`);
+    console.log(`${num1} - ${num2} = ${subtracao}`);
+    console.log(`${num1} x ${num2} = ${multiplicacao}`);
+    console.log(`${num1} / ${num2} = ${divisao}`);
+}
+
+realizarOperacoes(4, 5);
 
